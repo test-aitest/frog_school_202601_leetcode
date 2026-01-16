e1e1d73d41791d1d5187c26b71bd16a2853b66db
yuki-f-saka <ffffna0lucky+github@gmail.com>
2026-01-17 00:04:01 +0900

    Solve add-two-numbers.py

--- Yuki_Funasaka/neetcode150/linked_list/add-two-numbers.py
+++ Yuki_Funasaka/neetcode150/linked_list/add-two-numbers.py
@@ -43,3 +43,50 @@ class Solution:
         we take just O(m+n) time complexity when we do that way.
         m is l1 length and n is l2 length.
         """
+
+        """
+        First solution is wrong.
+        NO:
+        ・while cur1 and cur2
+        ・Only one time link the tail to carry
+
+        cur1 = l1
+        cur2 = l2
+        carry = 0
+        dummy = res = ListNode()
+        while cur1 and cur2:
+            sum = cur1.val + cur2.val + carry
+            if sum > 9:
+                carry = 1
+                sum = sum - 10
+            else:
+                carry = 0
+            res.next = ListNode(sum)
+            res = res.next
+            cur1 = cur1.next
+            cur2 = cur2.next
+        if carry == 1:
+            res.next = ListNode(carry)
+        return dummy.next
+        """
+
+        cur1, cur2 = l1, l2
+        carry = 0
+        dummy = ListNode(0)
+        tail = dummy
+
+        while cur1 or cur2 or carry:
+            v1 = cur1.val if cur1 else 0
+            v2 = cur2.val if cur2 else 0
+
+            s = v1 + v2 + carry
+            carry = s // 10
+            digit = s % 10
+
+            tail.next = ListNode(digit)
+            tail = tail.next
+
+            cur1 = cur1.next if cur1 else None
+            cur2 = cur2.next if cur2 else None
+
+        return dummy.next
