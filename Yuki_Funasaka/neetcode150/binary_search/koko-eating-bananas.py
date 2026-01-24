730b16bc5b21e7558b79bc0f2c86eb314f670ec2
yuki-f-saka <ffffna0lucky+github@gmail.com>
2026-01-24 21:23:14 +0900

    Understanding minEatingSpeed problem

--- Yuki_Funasaka/neetcode150/binary_search/koko-eating-bananas.py
+++ Yuki_Funasaka/neetcode150/binary_search/koko-eating-bananas.py
@@ -19,7 +19,33 @@ https://neetcode.io/problems/eating-bananas/question?list=neetcode150
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         """
-        k is the number of bananas you can eat during 1 hour. This is speed of eating bananas.
+        k is the number of bananas you can eat during 1 hour.
+        This is speed of eating bananas.
         you have to return minimum k.
-        Maybe you can use binary search algorighm. You look minimum k. l ~ k ~ r.
+        maybe you can use binary search algorithm,
+        you look minimum k between left and right pointers.
+
+        piles.length <= h <= 1,000,000
+        -> maximum k is max(piles). Because if k is max(piles), you take only len(piles) hours.
+        ex. piles = [25,10,23,4], max(piles)=25, len(piles)=4, if k is 25, you only take 4 hours.
+        That's why k is moving between 1 to max(piles).
+        k couldn't be over max(piles) because that's all true if k is over max(piles).
         """
+        l, r = 1, max(piles)
+        while l <= r:
+            mid = l + ((r - l) // 2)
+            if self.canEatAllBananas(piles, h, mid):
+                r = mid - 1
+            else:
+                l = mid + 1
+        return l
+    
+    def canEatAllBananas(self, piles: List[int], h: int, k: int) -> bool:
+        hours = 0
+        for b in piles:
+            hours += b // k
+            if b % k != 0:
+                hours += 1
+            if hours > h:
+                return False
+        return hours <= h
