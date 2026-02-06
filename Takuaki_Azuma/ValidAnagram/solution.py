71861d2452c8206a411063193c47b54110f427d6
Takuaki Azuma <tatennisku@gmail.com>
2026-02-07 08:25:39 +0900

    feat(taku): solve valid anagram

--- Takuaki_Azuma/ValidAnagram/solution.py
+++ Takuaki_Azuma/ValidAnagram/solution.py
@@ -1,4 +1,28 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # TODO: Implement the solution
-        pass
+        
+        # check the length
+        if len(s) != len(t):
+            return False
+
+        #2 Initialize manual counter(dictionary)
+        count = {}
+
+        #3 count charactors in string 's'
+        for char in s:
+            if char in count:
+                count[char] += 1
+            else:
+                count[char] = 1
+        #4 subtract counts using string 't'
+        for char in t:
+            if char not in count:
+                return False
+            
+            count[char] -= 1
+
+            if count[char] < 0:
+                return False
+        
+        #If we pass all checks, it is an anagram
+        return True
